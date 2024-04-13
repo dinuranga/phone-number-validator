ede8bd6d23957b37a47c19844178553ada67ad6c
Maleesha Dinuranga <maleeshadmax@gmail.com>
2024-04-13 17:13:42 +0530

    Fixed the regular expression

--- server.js
+++ server.js
@@ -4,34 +4,34 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Middleware to parse URL-encoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const phoneNumberRegex = /^(?:\+94|94)0(?:70|71|74|75|76|77|78)[0-9]{7}$/;
+const mobileNumberRegex = /^(?:\+94|94)?0(?:70|71|74|75|76|77|78)[0-9]{7}$/;
 
-// Middleware function to validate phone numbers
-function validatePhoneNumber(req, res, next) {
-    const phoneNumber = req.body.phoneNumber;
-    if (!phoneNumber) {
-        return res.status(400).json({ error: 'Phone number is missing in the request body' });
-    } else if (!phoneNumberRegex.test(phoneNumber)) {
-        return res.status(400).json({ error: 'Invalid phone number format' });
+// Middleware function to validate mobile numbers
+function validatemobileNumber(req, res, next) {
+    const mobileNumber = req.body.mobileNumber;
+    if (!mobileNumber) {
+        return res.status(400).json({ error: 'Mobile number is missing in the request body' });
+    } else if (!mobileNumberRegex.test(mobileNumber)) {
+        return res.status(400).json({ error: 'Invalid mobile number format' });
     }
     next();
 }
 
 app.get("/", (req, res) => {
-    res.json({ message: "Sri Lanka Phone Number Validator" });
+    res.json({ message: "Sri Lanka mobile Number Validator" });
 });
 
-// Route to handle phone number validation
-app.post('/validateNumber', validatePhoneNumber, (req, res) => {
-    res.json({ message: 'Phone number is valid' });
+// Route to handle mobile number validation
+app.post('/validateNumber', validatemobileNumber, (req, res) => {
+    res.json({ message: 'Mobile number is valid' });
 });
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });

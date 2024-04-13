b42f7eca520b78cb51ac211a9d22132d5695c05a
Maleesha Dinuranga <maleeshadmax@gmail.com>
2024-04-13 18:48:12 +0530

    CORS

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import dotenv from "dotenv";
 import LK_router from "./router/LK_router.js"
 import rateLimiter from './middleware/rateLimiter.js'
+import cors from "cors";
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use(rateLimiter);
 
 app.use("/api/lk", LK_router);
